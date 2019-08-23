40ab1ba85dbf3a780ee51b3aef75258c345f317f
Denis Nadeau <nadeau1@acme1.llnl.gov>
2019-08-23 16:33:28 -0700

    first draft not time axis fix

--- analysis_data_preprocess/process_ISLSCPII_COMP_RUNOFF.py
+++ analysis_data_preprocess/process_ISLSCPII_COMP_RUNOFF.py
@@ -9,63 +9,106 @@ import MV2
 import glob
 import numpy
 import cdutil
+from cdms2.tvariable import TransientVariable
+
+def writeANNFiles(data, year):
+    out_file = cdms2.open(data.id+"_v0.1_ANN_climo.nc", mode='w')
+    #Extrac information 
+    out_file.write(year)
+    out_file.close()
 
 
-basedir = '/p/user_pub/e3sm/zhang40/analysis_data_e3sm_diags/ISLSCPII_GRDC/'
-datapath = basedir  + 'original_data/data/comp_runoff_hdeg/'
-file_name = 'comp_runoff_hd'
+def writeSEASONFiles(data, seasonalcycle):
+    seasons = ['DJF','MAM','JJA','SON']
+    for i in range(0,len(seasons)):
+        out_file = cdms2.open(data.id+"_v0.1_"+seasons[i]+"_climo.nc", mode='w')
+        #Extrac information 
+        axes, attributes, id, grid = extractMetadata(seasonalcycle)
+        seasonaltime = seasonalcycle.getTime()
+    
+        # Create new data with new time axis
+        seasonalcycleMonth = seasonalcycle[i].asma()[MV2.newaxis, :]
+        seasonalcycleMonth.units = data.units   #converted from mm/month
+        seasonalcycleMonth.id = data.id #runoff flux for cmip
 
-start_time = '198601'
-end_time = '199512'
-#Missing data cells are given the value of -99 over water, and -88 over land 
-#(mainly Greenland and Antarctica).
-missing = -99
+        # Extract time axis with boudns
+        time = cdms2.createAxis([seasonaltime[i]])
+        time.units = seasonaltime.units
+        time._bounds_ = seasonaltime._bounds_[i]
+        time.designateTime()
+        time.id = "time"
 
-nlat = 360
-nlon = 720
-#nlat = 180
-#nlon =360
+        # Create anew cdms2 Variable
+        axes[0] = time
+        outdata = TransientVariable( seasonalcycleMonth, axes=axes, attributes=attributes, id=data.id, grid=grid)
+        out_file.write(outdata)
+        out_file.close()
 
-#Create axes, and a variable to hold 12 months of data
-lat = cdms2.createAxis([-89.25 + 0.5*i for i in range(nlat)],id='lat')
-lon = cdms2.createAxis([-179.75 + 0.5*i for i in range(nlon)],id='lon')
-out_file = cdms2.open(basedir + 'time_series/QRUNOFF_' + start_time + '_' + end_time + '.nc','w')
-start = "1986-01-01"
-nTimes = 12 * 10
-time = cdms2.createAxis(range(nTimes))
-time.units = "months since {}".format(start)
-time.designateTime()
-time.id = "time"
-data = MV2.zeros([nTimes, nlat, nlon])
+def writeCLIMOFiles(data, annualcycle):
+    for i in range(0,len(annualcycle.getTime())):
+        out_file = cdms2.open(data.id+"_v0.1_"+"{:02d}".format(i+1)+"_climo.nc", mode='w')
+        #Extrac information 
+        axes, attributes, id, grid = extractMetadata(annualcycle)
+        annualtime = annualcycle.getTime()
+    
+        # Create new data with new time axis
+        annualcycleMonth = annualcycle[i].asma()[MV2.newaxis, :]
+        annualcycleMonth.units = data.units   #converted from mm/month
+        annualcycleMonth.id = data.id #runoff flux for cmip
+
+        # Extract time axis with boudns
+        time = cdms2.createAxis([annualtime[i]])
+        time.units = annualtime.units
+        time._bounds_ = annualtime._bounds_[i]
+        time.designateTime()
+        time.id = "time"
+
+        # Create anew cdms2 Variable
+        axes[0] = time
+        outdata = TransientVariable( annualcycleMonth, axes=axes, attributes=attributes, id=data.id, grid=grid)
+        out_file.write(outdata)
+        out_file.close()
+
+def extractMetadata(a, axes=None, attributes=None,
+                     id=None, omit=None, omitall=False):
+    """Extract axes, attributes, id from 'a', if arg is None."""
+    resultgrid = None
+    from cdms2.avariable import AbstractVariable
+    from cdms2.avariable import AbstractRectGrid
+    if isinstance(a, AbstractVariable):
+        if axes is None:
+            axes = a.getAxisList(omit=omit)
+        if omitall:
+            axes = None
+        if attributes is None:
+            attributes = a.attributes
+        if id is None:
+            id = "variable_%i" % cdms2.tvariable.TransientVariable.variable_count
+            cdms2.tvariable.TransientVariable.variable_count += 1
+
+        # If the grid is rectilinear, don't return an explicit grid: it's implicitly defined
+        # by the axes.
+        resultgrid = a.getGrid()
+        if (resultgrid is None) or (isinstance(
+                resultgrid, AbstractRectGrid)) or (axes is None):
+            resultgrid = None
+
+        # If the omitted axis was associated with the grid, the result will not
+        # be gridded.
+        elif (omit is not None) and (resultgrid is not None) and (a.getAxis(omit) in resultgrid.getAxisList()):
+            resultgrid = None
+
+    return axes, attributes, id, resultgrid
 
 
-print(data.shape)
+in_file = cdms2.open('/p/user_pub/e3sm/zhang40/analysis_data_e3sm_diags/ISLSCPII_GRDC/time_series/QRUNOFF_198601_199512.nc','r')
+data=in_file('QRUNOFF')
+annualcycle=cdutil.ANNUALCYCLE.climatology(data, criteriaarg = [0.99,None])
+seasonalcycle = cdutil.SEASONALCYCLE.climatology(data, criteriaarg = [0.99,None])
+ANN = cdutil.YEAR.climatology(data, criteriaarg = [0.99,None])
+writeCLIMOFiles(data, annualcycle)
+writeSEASONFiles(data, seasonalcycle)
+writeANNFiles(data, ANN)
 
-count = 0
-for year in range(1986,1996):
-    for month in range(12):
-        imon = '{0:02d}'.format(month+1)
-        fname = datapath + file_name +'_' + str(year)+imon+'.asc'
-        
-        data[count,:,:] = numpy.loadtxt(fname, skiprows=6)
-        count = count+1
-
-print(count)
-data = MV2.masked_where(data == missing, data)
-out_data = cdms2.createVariable(data, axes =[time,lat,lon], id = "mrro")
-out_data.units = "mm/month"   #converted from mm/month
-out_data.id = "QRUNOFF" #runoff flux for cmip
-cdutil.setTimeBoundsMonthly(out_data)
-out_file.dataname = 'ISLSCP-GRDC'
-out_file.long_dataname = "ISLSCP II UNH/GRDC Composite Monthly Runoff"
-out_file.comment = "The files in this data set comprise river discharge point data from the Global Runoff Data Centre (GRDC) for all 390 stations with measurements in the period 1986-1995 and 2) global, gridded, monthly composite runoff time series for the period 1986-1995, produced at UNH using the GRDC river discharge data. The original data was provided in ASCII format, it is converted to netcdf for using in e3sm_diags"
-out_file.years = "1986-1995"
-out_file.write(out_data)
-
-
-
-#import cdutil
-#seasonal = cdutil.DJF.climatology(out_file)
-#print(seasonal)
 
 
