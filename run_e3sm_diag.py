8ae7c9eeacaf5b43a36ebf7a1b93e6f0007c5e4e
jingxianwen <jingxianwen@live.cn>
2020-03-17 12:58:10 -0700

    add RRTMGP_UMRad_vs_standar

--- run_e3sm_diag.py
+++ run_e3sm_diag.py
@@ -12,7 +12,7 @@ param.run_type='model_vs_model' #available: 'model_vs_obs'(default), 'model_vs_m
 #param.reference_data_path = '/global/project/projectdirs/acme/acme_diags/obs_for_e3sm_diags/climatology/'
 #param.reference_title = 'OBS'# will show on plots
 param.reference_data_path = '/global/cscratch1/sd/xianwen/E3SM_simulations/E3SM_v2_alpha_AMIP_RRTMGP.ne30_ne30.cori-knl/archive/climo/'
-param.reference_name = 'E3SM_v2_alpha_AMIP_RRTMGP.ne30_ne30.cori-knl' #match output file name
+#param.reference_name = 'E3SM_v2_alpha_AMIP_RRTMGP.ne30_ne30.cori-knl' #match output file name
 param.reference_title = 'RRTMGP_standard'# will show on plots
 
 
@@ -39,7 +39,8 @@ param.num_workers = 32
 param.save_netcdf=False
 
 #use below to run all core sets of diags:
-runner.sets_to_run = ['lat_lon','zonal_mean_xy', 'zonal_mean_2d', 'polar', 'cosp_histogram', 'meridional_mean_2d']
+#runner.sets_to_run = ['lat_lon','zonal_mean_xy', 'zonal_mean_2d', 'polar', 'cosp_histogram', 'meridional_mean_2d']
+runner.sets_to_run = ['lat_lon','zonal_mean_xy', 'zonal_mean_2d', 'polar']
 #use below to run lat_lon map only
 #runner.sets_to_run = ['lat_lon']
 
